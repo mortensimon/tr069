4fbb5d4de802461a8513bb589c77013adc21fc09
morten <mortensimon@gmail.com>
2015-01-29 20:09:27 +0100

    v3.1.2 (2015-01-29) - bug: TransferCompleteResponse did not return the CWMP:ID correctly

--- src/com/owera/xaps/tr069/Provisioning.java
+++ src/com/owera/xaps/tr069/Provisioning.java
@@ -51,7 +51,7 @@ public class Provisioning extends HttpServlet {
 
 	private static final long serialVersionUID = -3020450686422484143L;
 
-	public static final String VERSION = "3.1.1";
+	public static final String VERSION = "3.1.2";
 
 //	private static BackgroundProcesses backgroundProcesses = new BackgroundProcesses();
 
